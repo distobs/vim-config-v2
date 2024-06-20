75d01921b15fe833132dc0833ea65a7473e542ed
distobs <cuppotatocake@gmail.com>
2024-06-20 12:13:36 -0300

    love CoC

--- setup.sh
+++ setup.sh
@@ -11,3 +11,4 @@ mkdir -p $plugdir ; cd $plugdir ; mkdir 'start' 'opt'
 
 git clone --branch=master --depth=1 https://github.com/morhetz/gruvbox 'start/gruvbox'
 git clone --branch=master --depth=1 https://github.com/mattn/emmet-vim 'opt/emmet-vim'
+git clone --depth=1 --branch=release https://github.com/neoclide/coc.nvim 'opt/coc.nvim'
